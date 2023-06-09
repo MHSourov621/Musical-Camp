dd5cd9355a260202dbe5aaf951a9920c7abd472b
MHSourov621 <mhsourov621@gmail.com>
2023-06-09 23:28:07 +0600

    instructor added

--- src/component/InstructorCard/InstructorCard.jsx
+++ src/component/InstructorCard/InstructorCard.jsx
@@ -4,10 +4,10 @@ const InstructorCard = ({ instructor }) => {
     const { name, email, image } = instructor;
     return (
         <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img className="w-[400px] h-[500px]" src={image} alt="Movie" /></figure>
+            <figure><img className="w-[300px] h-[350px]" src={image} alt="Movie" /></figure>
             <div className="card-body text-black">
-                <h2 className="card-title">{name}</h2>
-                <p>Email: {email}</p>
+                <h2 className="card-title text-4xl font-bold text-blue-700">{name}</h2>
+                <p className="text-xl font-semibold mt-6">Email: {email}</p>
                 <div className="card-actions justify-end">
                     <button className="btn bg-blue-700 border-blue-500 border-2 border-r-0 border-t-0 hover:bg-blue-600 text-white font-semibold">See Classes</button>
                 </div>
